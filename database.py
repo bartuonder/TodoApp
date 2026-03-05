b3fa47fc112d0084e3212d367c1f20337a1814af
Bartu Onder <onderbartu8@gmail.com>
2026-03-05 21:01:53 +0300

    feat: production DBMS's are used (mysql and postgresql).

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./todos_app.db"
+DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoappdatabase"  #"postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
