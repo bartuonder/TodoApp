5e468294b8c1cffcfc9814b0eaf4fe24a3a33fab
Bartu Onder <onderbartu8@gmail.com>
2026-03-06 17:26:34 +0300

    feat: alembic is added to project

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoappdatabase"  #"postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
